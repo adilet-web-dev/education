178b0cf1e8b1cf8423389ddb0404c3120a3d64ee
mrAdiletDev <adiletdj19@gmail.com>
2022-03-23 21:01:20 +0600

    ADD few fields to Course CREATE Homework and HomeworkTask models

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("config.api_router")),
     path('api/', include("config.swagger")),
+    path('api/', include("config.swagger")),
     path('api/', include("config.api_router")),
     path('api/users/', include("apps.users.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
