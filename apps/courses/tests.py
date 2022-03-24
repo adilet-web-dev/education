aae26fa200806589d249a69925621655017bde98
mrAdiletDev <adiletdj19@gmail.com>
2022-03-24 16:46:07 +0600

    ADD homework task and homework creation ADD file upload support

--- apps/courses/tests.py
+++ apps/courses/tests.py
@@ -1,12 +1,12 @@
+from pathlib import Path
+
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from django.utils import timezone
 
-from config.settings import BASE_DIR
-
 from apps.users.factories import UserWithProfileFactory
 from .models import Course, HomeworkTask
-from .factories import CourseFactory, HomeworkTaskFactory
+from .factories import CourseFactory, HomeworkTaskFactory, HomeworkFactory
 
 
 # Create your tests here.
@@ -28,8 +28,21 @@ class CourseViewSetAPITest(APITestCase):
         response = self.client.post("/api/courses/", payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
         self.assertTrue(Course.objects.filter(name=payload["name"]).exists())
+
+
+class UploadCourseFilesAPITest(APITestCase):
+    def test_it_uploads_file(self):
+        course = CourseFactory()
+        with open(f"{Path(__file__).resolve().parent}/test_files/dummy.zip", "rb") as file:
+            response = self.client.post(
+                f"/api/courses/{course.id}/upload-file/",
+                {"file": file}
+            )
+
+        self.assertEqual(response.status_code, 201)
+        course.refresh_from_db()
+        self.assertNotEqual(course.file, None)
 
 
 class HomeworkTaskCreateTest(APITestCase):
@@ -69,16 +82,57 @@ class HomeworkTaskCreateTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
+class UploadHomeworkTaskFilesAPITest(APITestCase):
+    def test_it_uploads_file(self):
+        course = CourseFactory()
+        homework_task = HomeworkTaskFactory(course=course)
+        with open(f"{Path(__file__).resolve().parent}/test_files/dummy.zip", "rb") as file:
+            response = self.client.post(
+                f"/api/courses/{course.id}/homework-tasks/{homework_task.id}/upload-file/",
+                {"file": file}
+            )
+
+        self.assertEqual(response.status_code, 201)
+        homework_task.refresh_from_db()
+        self.assertNotEqual(homework_task.file, None)
+
+
 class HomeworkCreateTest(APITestCase):
     def setUp(self) -> None:
-        self.user = UserWithProfileFactory()
-        self.client.force_login(self.user)
         self.course = CourseFactory()
         self.homework_task = HomeworkTaskFactory(course=self.course)
         self.url = f"/api/courses/{self.course.id}/homework-tasks/{self.homework_task.id}/homeworks/"
 
     def test_it_creates_homework(self):
+        student = UserWithProfileFactory()
+        client = APIClient()
+        client.force_login(student)
+        self.course.students.add(student.profile)
 
-        response = self.client.post(self.url, {"text": "some text"})
-
+        response = client.post(self.url, {"text": "some text"})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_non_courses_student_user_cannot_send_homework(self):
+        client = APIClient()
+        user = UserWithProfileFactory()
+        client.force_login(user)
+
+        response = client.post(self.url, {"text": "some text"})
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class UploadHomeworkFilesAPITest(APITestCase):
+    def test_it_uploads_file(self):
+        course = CourseFactory()
+        homework_task = HomeworkTaskFactory(course=course)
+        homework = HomeworkFactory(homework_task=homework_task)
+        with open(f"{Path(__file__).resolve().parent}/test_files/dummy.zip", "rb") as file:
+            response = self.client.post(
+                f"/api/courses/{course.id}/homework-tasks/{homework_task.id}/homeworks/{homework.id}/upload-file/",
+                {"file": file}
+            )
+
+        self.assertEqual(response.status_code, 201)
+        homework.refresh_from_db()
+        self.assertNotEqual(homework.file, None)
