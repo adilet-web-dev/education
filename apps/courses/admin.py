9bf98b920381de1d428030d8b442de925f001904
mrAdiletDev <adiletdj19@gmail.com>
2022-03-29 19:57:39 +0600

    ADD models to admin site

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Course, HomeworkTask, Homework
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(HomeworkTask)
+admin.site.register(Homework)
